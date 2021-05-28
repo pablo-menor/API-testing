e1883e79a98663ffef7a288f27437b436aa44968
pablo-menor <pablomenor2001@gmail.com>
2021-05-28 20:17:35 +0200

    connected to api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-'use strict'
+const url = "https://jsonplaceholder.typicode.com/users";
+const button = document.querySelector(".container");
+
+button.addEventListener('click', () =>{
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        console.log(json)
+    })
+})
